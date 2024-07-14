c135fe95c2ba3a4fd18116c5cda723e7bb07dfdd
Bảo Ngoc <baokhuong6996@gmail.com>
2024-07-15 02:46:36 +0700

    fix box

--- src/pages/Dashboard/Management.js
+++ src/pages/Dashboard/Management.js
@@ -1,11 +1,11 @@
 import React from "react";
 import "../../assets/styles/Dashboard/Management.scss"
-import { Box, Grid, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from "@mui/material";
+import { Box, Grid, ListItemIcon, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from "@mui/material";
 import { DataGrid } from "@mui/x-data-grid";
 import HeaderHello from "../../components/HeaderHello";
 import TableOrder from "../../components/Manager/TableOrder";
 import TableManufactureMonth from "../../components/Manager/TableManufactureMonth";
-
+import DescriptionIcon from '@mui/icons-material/Description';
 
 
 const Management = () => {
@@ -18,25 +18,50 @@ const Management = () => {
 
                 <Grid item xs={12} container spacing={2}>
                     <Grid item xs={3}>
-                        <Paper elevation={3}>
-                            <Box p={2}>
+                        <Paper elevation={3} sx={{ height: '100%' }}>
+                            <Box p={3}>
                                 <Typography variant="h4" sx={{ fontWeight: 'bold', textTransform: 'uppercase' }}>
                                     Tháng này đã sản xuất
                                 </Typography>
                                 <hr />
-                                <Typography variant="h4" sx={{ fontWeight: 'bold', textTransform: 'uppercase' }}>
+                                <Typography variant="h3" sx={{ fontWeight: 'bold', textTransform: 'uppercase' }}>
                                     50 cái
                                 </Typography>
                             </Box>
                         </Paper>
                     </Grid>
 
-                    <Grid item xs={9} >
+                    <Grid item xs={9}>
                         <Paper elevation={3} sx={{ height: '100%' }}>
-                            <Box p={2} sx={{ display: 'flex', gap: 2 }}>
-                            <Box sx={{ flex: 1, backgroundColor: 'primary.main', padding: 2, height: '100%' }}>Box 1</Box>
-                            <Box sx={{ flex: 1, backgroundColor: 'secondary.main', padding: 2, height: '100%' }}>Box 2</Box>
-                            <Box sx={{ flex: 1, backgroundColor: 'success.main', padding: 2, height: '100%' }}>Box 3</Box>
+                            <Typography variant="h3" sx={{ fontWeight: 'bold', textTransform: 'uppercase',padding: '5px' }}>
+                                Thông tin sản xuất
+                            </Typography>
+                            <hr />
+                            <Box p={2} sx={{ display: 'flex', gap: 3 }}>
+                                <Paper elevation={3}>
+                                    <Box sx={{ flex: 1, padding: 2, height: '100%' }}>
+                                        <Typography variant="h6" sx={{ fontWeight: 'bold', textTransform: 'uppercase' }}>kế hoạch sản xuất hôm nay</Typography>
+                                        <hr />
+                                        <Typography variant="h5" sx={{ fontWeight: 'bold', textTransform: 'uppercase' }}>24 đơn</Typography>
+                                    </Box>
+                                </Paper>
+
+                                <Paper elevation={3}>
+                                    <Box sx={{ flex: 1, padding: 2, height: '100%' }}>
+                                        <Typography variant="h6" sx={{ fontWeight: 'bold', textTransform: 'uppercase' }}>kế hoạch sản xuất hôm nay</Typography>
+                                        <hr />
+                                        <Typography variant="h5" sx={{ fontWeight: 'bold', textTransform: 'uppercase' }}>24 đơn</Typography>
+                                    </Box>
+                                </Paper>
+
+                                <Paper elevation={3}>
+                                    <Box sx={{ flex: 1, padding: 2, height: '100%' }}>
+                                        <Typography variant="h6" sx={{ fontWeight: 'bold', textTransform: 'uppercase' }}>kế hoạch sản xuất hôm nay</Typography>
+                                        <hr />
+                                        <Typography variant="h5" sx={{ fontWeight: 'bold', textTransform: 'uppercase' }}>24 đơn</Typography>
+                                    </Box>
+                                </Paper>
+
                             </Box>
                         </Paper>
                     </Grid>
